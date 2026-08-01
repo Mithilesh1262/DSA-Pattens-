25221f7d44b78bdec849473c49b9d70626d2b464
Mithilessh <mithilessh.ya01@gmail.com>
2026-08-01 15:38:54 +0530

    input, output, variables, maths, and run a program

---
+++ day_1.cpp
@@ -0,0 +1,22 @@
+// input, output, variables, maths 
+
+// 1. take  3 no. and print their prodducts..
+// 2. take 2 no.  and print their average..
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int  a;
+    int b;
+    int c;
+     cout<< "enter 3 no." << endl;
+
+     cin>>a>>b>>c;
+
+     int d = a*b*c;
+
+     cout<< d;
+
+}
+
