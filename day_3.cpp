05bd322b3fbcd664ab6dfe871827e1b229e2ecae
Mithilessh <mithilessh.ya01@gmail.com>
2026-08-02 04:13:56 +0530

    create a program double the no.  using function

---
+++ day_3.cpp
@@ -0,0 +1,20 @@
+// function 
+
+// return type 
+// function type
+// input type
+
+//  double the no.
+
+#include <iostream>
+using namespace std;
+int dub(int n)
+{
+    return n*2;
+} 
+int main()
+{
+int m = 10;  
+cout<< dub(m);
+    return 0;
+}
